15532fd4ecd18375815d95120570ab99b10c10c3
Vancarii <xnycwang@yahoo.com>
2025-03-28 15:20:51 -0700

    new resume and projects

--- app/pages/projects/page.tsx
+++ app/pages/projects/page.tsx
@@ -1,63 +1,23 @@
-// import React from "react";
-
-// const Projects: React.FC = () => {
-//   return (
-//     <>
-//       <div className="flex flex-col items-center justify-center min-h-screen space-y-8">
-//         {/* <h1 className="text-3xl font-bold font-mono">Projects</h1> */}
-//         <div className=" mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px]">
-//           <div className="h-[32px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[72px] rounded-s-lg"></div>
-//           <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
-//           <div className="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
-//           <div className="h-[64px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
-//           <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white dark:bg-gray-800">
-//             <img
-//               src="/app-screenshots/stario7.jpg"
-//               className="dark:hidden w-[272px] h-[572px]"
-//               alt="Stario"
-//             />
-//             <img
-//               src="/app-screenshots/stario7.jpg"
-//               className="hidden dark:block w-[272px] h-[572px]"
-//               alt="Stario"
-//             />
-//           </div>
-//         </div>
-//         <div className=" mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[8px] rounded-t-xl h-[172px] max-w-[301px] md:h-[294px] md:max-w-[512px]">
-//           <div className="rounded-lg overflow-hidden h-[156px] md:h-[278px] bg-white dark:bg-gray-800">
-//             <img
-//               src="/app-screenshots/whistlerBBT.png"
-//               className="dark:hidden h-[156px] md:h-[278px] w-full rounded-lg"
-//               alt=""
-//             />
-//             <img
-//               src="/app-screenshots/whistlerBBT.png"
-//               className="hidden dark:block h-[156px] md:h-[278px] w-full rounded-lg"
-//               alt=""
-//             />
-//           </div>
-//         </div>
-//         <div className="relative mx-auto bg-gray-900 dark:bg-gray-700 rounded-b-xl rounded-t-sm h-[17px] max-w-[351px] md:h-[21px] md:max-w-[597px]">
-//           <div className="absolute left-1/2 top-0 -translate-x-1/2 rounded-b-xl w-[56px] h-[5px] md:w-[96px] md:h-[8px] bg-gray-800"></div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Projects;
-
 import ProjectCard from "@/components/project-card";
 
 const projects = [
   {
-    title: "Whistler Bubble Tea Website",
+    title: "Whistler Bubble Tea",
     description:
       "A revamped website for a bubble tea shop in Whistler, BC, Canada.",
     imageUrl: "/app-screenshots/whistlerBBT.png",
     technologies: ["HTML", "CSS", "GoDaddy"],
     demoUrl: "https://whistlerbubbletea.ca",
-    sourceUrl: "https://github.com/Vancarii/whistlerbubbletea",
+    sourceUrl: "",
+  },
+  {
+    title: "GitGPT: OpenAI Code Assistant",
+    description:
+      "A React Native mobile application deployed on Vercel for a mobile simulator.",
+    imageUrl: "/app-screenshots/gitgpt.png",
+    technologies: ["NodeJS", "Expo", "OpenAI API"],
+    demoUrl: "https://gitgpt-amber.vercel.app",
+    sourceUrl: "https://github.com/Vancarii/gitgpt",
   },
   {
     title: "Molu Finance",
@@ -87,7 +47,16 @@ const projects = [
     sourceUrl: "https://github.com/AdamBahramiSchool/BusinessBeacon",
   },
   {
-    title: "Tic-Tac-Toe Agent",
+    title: "OpenAI Web Scraper and Lead Generation",
+    description:
+      "Streamlit-based web application designed to help users generate leads by finding business websites and extracting verified email addresses. ",
+    imageUrl: "/app-screenshots/scraper.jpeg",
+    technologies: ["Streamlit", "Python", "Pandas", "Open AI"],
+    demoUrl: "",
+    sourceUrl: "https://github.com/Vancarii/gpt-web-scraper",
+  },
+  {
+    title: "Tic-Tac-Toe AI Agents",
     description:
       "A Python script that uses the different pre-planning algorithms to play tic-tac-toe. Done as an assignment for CMPT310 at SFU.",
     imageUrl: "/app-screenshots/tictactoe.png",
